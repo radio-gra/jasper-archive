940246eb6d165850f39f0905fe8465180b75f262
dreux <dimonychan@gmail.com>
2019-12-26 17:52:53 +0300

    правки

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.sass']
+  styleUrls: ['./nav.component.sass'],
 })
 export class NavComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  public ngOnInit(): void {
-  }
-
+  public ngOnInit(): void {}
 }
